acbb1abcbef3ac4ec50351e26b4dfeb58de1822d
Cristian <cristiannicolas32@hotmail.com>
2020-12-03 19:53:22 -0300

    Subo par e impar

---
+++ Par_O_Impar.js
@@ -0,0 +1,8 @@
+function parImpar(numero) {
+    if (numero == 0){
+    return console.log("Error el numero es 0")
+    }else numero % 2 ? console.log("El numero es par") : console.log("El numero es Impar");
+}
+parImpar(0);
+parImpar(2)
+parImpar(3)
